bfcbde70fa000b3afbd4bea2601e241b7826cb54
shangchunli <1503651223@qq.com>
2019-12-12 20:52:22 +0800

    message

--- child-care/src/container/wiki/Chaowa.js
+++ child-care/src/container/wiki/Chaowa.js
@@ -1,81 +1,15 @@
 import React, { Component } from 'react'
-import { NavBar, Icon,List} from 'antd-mobile';
+import { NavBar, Icon} from 'antd-mobile';
 import '../Wiki.css'
-import cookie from 'react-cookies'
-
 
 
 export default class Chaowa extends Component {
     goBack=()=>{
         window.history.go(-1);
     }
-    constructor(){
-        super();
-        this.state={
-            data:[],
-            isKeep:false,
-            chapterId:[],
-            userId:cookie.load('userId')
-        }
-    }
-    
-    change1=(idx,e)=>{
-        let b=this.state.isKeep;
-        this.setState({
-            isKeep:!b,
-            chapterId:idx+1
-
-        })
-        console.log(cookie.load('chapterId'));
-        cookie.save("chapterId",this.state.chapterId);
-        if(this.state.isKeep==true){
-            
-            e.target.src='https://s2.ax1x.com/2019/12/11/QrKe4s.png'
-            // alert(this.state.userId);
-           
-                fetch('http://192.168.43.217:5001/cookie',{
-                        method: 'POST',//post请求 
-                        headers: { 
-                        'Content-Type': 'application/json;charset=UTF-8' 
-                        }, 
-                        body: JSON.stringify({
-                            userId:this.state.userId,  
-                            chapterId:this.state.chapterId                    
-                        })                    
-                })
-                .then(res=>res.json())
-                .then((res)=>{
-                    console.log(res);
-                   
-                })
-               
-           
-        }else{
-            e.target.src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'
-        }
-        console.log(this.state.chapterId);
-    }
-   
-    componentDidMount(){
-        fetch('http://192.168.43.217:5001/jibing',{
-            method: 'GET', 
-            headers: { 
-                'Content-Type': 'application/json;charset=UTF-8' 
-            }, 
-        })
-        .then(res=>res.json())
-        .then((res)=>{
-            console.log(res);
-            this.setState({
-                data:res
-            });
-            // console.log(this.state.data)
-        })
-       
-    }
     render() {
         return (
-            <div>
+            <ul>
                 <NavBar
                     mode="light"
                     icon={<Icon type="left" onClick={this.goBack}/>}
@@ -85,41 +19,7 @@ export default class Chaowa extends Component {
                     >
                         潮娃穿搭
                 </NavBar>
-                <List>
-                            {
-                                (this.state.data||[]).map((item,idx)=>{
-                                  
-                                    return(
-                                        <List>
-                                            <List.Item style={{paddingTop:10,color:"#000"}}>
-                                               
-                                                <List.Item.Brief style={{color:"#000",width:'75%',float:'left'}}>
-                                                    {item.title}
-                                                    {/* 什么是产后抑郁症
-                                                    产后抑郁症是女性精神障碍中... */}
-                                                </List.Item.Brief>
-                                                <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg"
-                                                        style={{
-                                                        width:'20%',height:'10%'}}
-                                                />
-                                                <List.Item.Brief >
-                                                    丫丫妈妈
-                                                    <span style={{marginLeft:10}}>11月12日</span>
-                                                    <span  style={{marginLeft:20}} >
-                                                        <img src={(cookie.load('chapterId')==idx+1)
-                                                                ?'https://s2.ax1x.com/2019/12/11/QrKe4s.png'
-                                                                :"https://s2.ax1x.com/2019/12/04/Q1fu7T.png"}
-                                                            onClick={(e)=>this.change1(idx,e)} alt='收藏'/>
-                                                        {/* {cookie.load('chapterId')} */}
-                                                    </span>
-                                                </List.Item.Brief>
-                                            </List.Item>
-                                        </List>
-                                    )
-                                })
-                            }
-                        </List>
-                {/* <li style={{marginTop:'5%',marginLeft:'9%'}}>    
+                <li style={{marginTop:'5%',marginLeft:'9%'}}>    
                     <div className='tiezi'style={{ }}>
                         <img  src='https://s2.ax1x.com/2019/12/04/Q1tlfe.jpg'/>
                     <p>潮娃穿搭指南，10岁裴佳欣甜美型</p>
@@ -153,8 +53,8 @@ export default class Chaowa extends Component {
                         <img style={{height:20,width:20,float:'right',marginTop:-20,marginRight:10}}  
                     src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' />                    </div>
 
-                </li> */}
-            </div>
+                </li>
+            </ul>
                
         )
     }
--- child-care/src/container/wiki/Inoculation.js
+++ child-care/src/container/wiki/Inoculation.js
@@ -1,273 +1,121 @@
 import React, { Component } from 'react'
-// import { Flex } from 'antd-mobile';
-import cookie from 'react-cookies'
-
-import { NavBar,List,Icon } from 'antd-mobile';
-
-// const Item = List.Item;
+import { Flex } from 'antd-mobile';
 
 export default class Inoculation extends Component {
-    constructor(){
-        super();
-        this.state={
-            data:[],
-            isKeep:false,
-            chapterId:[],
-            userId:cookie.load('userId')
-        }
-    }
-    
-    change1=(idx,e)=>{
-        let b=this.state.isKeep;
-        this.setState({
-            isKeep:!b,
-            chapterId:idx+1
-
-        })
-        console.log(cookie.load('chapterId'));
-        cookie.save("chapterId",this.state.chapterId);
-        if(this.state.isKeep==true){
-            
-            e.target.src='https://s2.ax1x.com/2019/12/11/QrKe4s.png'
-            // alert(this.state.userId);
-           
-                fetch('http://192.168.43.217:5001/cookie',{
-                        method: 'POST',//post请求 
-                        headers: { 
-                        'Content-Type': 'application/json;charset=UTF-8' 
-                        }, 
-                        body: JSON.stringify({
-                            userId:this.state.userId,  
-                            chapterId:this.state.chapterId                    
-                        })                    
-                })
-                .then(res=>res.json())
-                .then((res)=>{
-                    console.log(res);
-                   
-                })
-               
-           
-        }else{
-            e.target.src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'
-        }
-        console.log(this.state.chapterId);
-    }
-   
-    componentDidMount(){
-        fetch('http://192.168.43.217:5001/jibing',{
-            method: 'GET', 
-            headers: { 
-                'Content-Type': 'application/json;charset=UTF-8' 
-            }, 
-        })
-        .then(res=>res.json())
-        .then((res)=>{
-            console.log(res);
-            this.setState({
-                data:res
-            });
-            // console.log(this.state.data)
-        })
-       
-    }
-    
     render() {
         return (
-            
-                    <div>
+            <div>
+                <Flex>
+                    <Flex.Item>
+                        <div style={{width:'100%'}}>
+                            <img src="https://s2.ax1x.com/2019/12/04/Q1hib6.png" style={{width:'100%',height:'160px'}}/>
+                            <p style={{width:'100%',marginBottom:'2%'}}>
+                                宝宝从出生后至12岁，都要进行...<br/>
+                            </p>
+                            <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
+                            <span>
+                                <img style={{
+                                    width:'10%',
+                                    height:'15%',
+                                    float:'right',
+                                    marginRight:'5%'
+                                }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' alt='收藏' />
+                            </span>
+                        </div>
+                    </Flex.Item>
+                    <Flex.Item>
+                        <div style={{width:'100%'}}>
+                            <img src="https://s2.ax1x.com/2019/12/04/Q1hib6.png" style={{width:'100%',height:'160px'}}/>
+                            <p style={{width:'100%'}}>
+                                脊髓灰质炎糖丸—第...<br/>
+                            </p>
+                            <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
+                            <span>
+                                <img style={{
+                                    width:'10%',
+                                    height:'15%',
+                                    float:'right',
+                                    marginRight:'5%'
+                                }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' alt='收藏' />
+                            </span>
+                        </div>
+                    </Flex.Item>
+                </Flex>
 
-                        <List>
-                            {
-                                (this.state.data||[]).map((item,idx)=>{
-                                  
-                                    return(
-                                        <List>
-                                            <List.Item style={{paddingTop:10,color:"#000"}}>
-                                               
-                                                <List.Item.Brief style={{color:"#000",width:'75%',float:'left'}}>
-                                                    {item.title}
-                                                    {/* 什么是产后抑郁症
-                                                    产后抑郁症是女性精神障碍中... */}
-                                                </List.Item.Brief>
-                                                <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg"
-                                                        style={{
-                                                        width:'20%',height:'10%'}}
-                                                />
-                                                <List.Item.Brief >
-                                                    丫丫妈妈
-                                                    <span style={{marginLeft:10}}>11月12日</span>
-                                                    <span  style={{marginLeft:20}} >
-                                                        <img src={(cookie.load('chapterId')==idx+1)
-                                                                ?'https://s2.ax1x.com/2019/12/11/QrKe4s.png'
-                                                                :"https://s2.ax1x.com/2019/12/04/Q1fu7T.png"}
-                                                            onClick={(e)=>this.change1(idx,e)} alt='收藏'/>
-                                                        {/* {cookie.load('chapterId')} */}
-                                                    </span>
-                                                </List.Item.Brief>
-                                            </List.Item>
-                                        </List>
-                                    )
-                                })
-                            }
-                        </List>
-                       
-                    {/* <Flex>
-                        <Flex.Item>
-                            <div style={{width:'100%'}}>
-                                <img src="https://s2.ax1x.com/2019/12/04/Q1fyjI.png" style={{width:'100%',height:'160px'}}/>
-                                <p style={{width:'100%',marginBottom:'2%'}}> 
-                                   {}<br/>
-                                </p>
-                                <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
-                                <span>
-                                    <img style={{
-                                        width:'10%',
-                                        height:'15%',
-                                        float:'right',
-                                        marginRight:'5%'
-                                    }} 
-                                    src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' alt='收藏'
-                                    onClick={this.change}
-                                    />
-                                </span>
-                            </div>
-                        </Flex.Item>
-                        <Flex.Item>
-                            <div style={{width:'100%'}}>
-                                <img src="https://s2.ax1x.com/2019/12/04/Q1fyjI.png" style={{width:'100%',height:'160px'}}/>
-                                <p style={{width:'100%'}}>
-                                    冬季腹泻，你的孩子中招了吗...<br/>
-                                </p>
-                                <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
-                                <span>
-                                    <img style={{
-                                        width:'10%',
-                                        height:'15%',
-                                        float:'right',
-                                        marginRight:'5%'
-                                    }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'onClick={this.change} alt='收藏' />
-                                </span>
-                            </div>
-                        </Flex.Item>
-                    </Flex>
+                <Flex>
+                    <Flex.Item>
+                        <div style={{width:'100%'}}>
+                            <img src="https://s2.ax1x.com/2019/12/04/Q1hib6.png" style={{width:'100%',height:'160px'}}/>
+                            <p style={{width:'100%',marginBottom:'2%'}}>
+                                宝宝从出生后至12岁，都要进行...<br/>
+                            </p>
+                            <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
+                            <span>
+                                <img style={{
+                                    width:'10%',
+                                    height:'15%',
+                                    float:'right',
+                                    marginRight:'5%'
+                                }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' alt='收藏' />
+                            </span>
+                        </div>
+                    </Flex.Item>
+                    <Flex.Item>
+                        <div style={{width:'100%'}}>
+                            <img src="https://s2.ax1x.com/2019/12/04/Q1hib6.png" style={{width:'100%',height:'160px'}}/>
+                            <p style={{width:'100%'}}>
+                                脊髓灰质炎糖丸—第...<br/>
+                            </p>
+                            <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
+                            <span>
+                                <img style={{
+                                    width:'10%',
+                                    height:'15%',
+                                    float:'right',
+                                    marginRight:'5%'
+                                }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' alt='收藏' />
+                            </span>
+                        </div>
+                    </Flex.Item>
+                </Flex>
 
-                    <Flex>
-                        <Flex.Item>
-                            <div style={{width:'100%'}}>
-                                <img src="https://s2.ax1x.com/2019/12/04/Q1fyjI.png" style={{width:'100%',height:'160px'}}/>
-                                <p style={{width:'100%',marginBottom:'2%'}}>
-                                    冬季腹泻，你的孩子中招了吗...<br/>
-                                </p>
-                                <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
-                                <span>
-                                    <img style={{
-                                        width:'10%',
-                                        height:'15%',
-                                        float:'right',
-                                        marginRight:'5%'
-                                    }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' onClick={this.change}alt='收藏' />
-                                </span>
-                            </div>
-                        </Flex.Item>
-                        <Flex.Item>
-                            <div style={{width:'100%'}}>
-                                <img src="https://s2.ax1x.com/2019/12/04/Q1fyjI.png" style={{width:'100%',height:'160px'}}/>
-                                <p style={{width:'100%'}}>
-                                    冬季腹泻，你的孩子中招了吗...<br/>
-                                </p>
-                                <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
-                                <span>
-                                    <img style={{
-                                        width:'10%',
-                                        height:'15%',
-                                        float:'right',
-                                        marginRight:'5%'
-                                    }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'onClick={this.change} alt='收藏' />
-                                </span>
-                            </div>
-                        </Flex.Item>
-                    </Flex>
-
-                    <Flex>
-                        <Flex.Item>
-                            <div style={{width:'100%'}}>
-                                <img src="https://s2.ax1x.com/2019/12/04/Q1fyjI.png" style={{width:'100%',height:'160px'}}/>
-                                <p style={{width:'100%',marginBottom:'2%'}}>
-                                    冬季腹泻，你的孩子中招了吗...<br/>
-                                </p>
-                                <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
-                                <span>
-                                    <img style={{
-                                        width:'10%',
-                                        height:'15%',
-                                        float:'right',
-                                        marginRight:'5%'
-                                    }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'onClick={this.change} alt='收藏' />
-                                </span>
-                            </div>
-                        </Flex.Item>
-                        <Flex.Item>
-                            <div style={{width:'100%'}}>
-                                <img src="https://s2.ax1x.com/2019/12/04/Q1fyjI.png" style={{width:'100%',height:'160px'}}/>
-                                <p style={{width:'100%'}}>
-                                    冬季腹泻，你的孩子中招了吗...<br/>
-                                </p>
-                                <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
-                                <span>
-                                    <img style={{
-                                        width:'10%',
-                                        height:'15%',
-                                        float:'right',
-                                        marginRight:'5%'
-                                    }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'onClick={this.change} alt='收藏' />
-                                </span>
-                            </div>
-                        </Flex.Item>
-                    </Flex>
-
-                    <Flex>
-                        <Flex.Item>
-                            <div style={{width:'100%'}}>
-                                <img src="https://s2.ax1x.com/2019/12/04/Q1fyjI.png" style={{width:'100%',height:'160px'}}/>
-                                <p style={{width:'100%',marginBottom:'2%'}}>
-                                    冬季腹泻，你的孩子中招了吗...<br/>
-                                </p>
-                                <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
-                                <span>
-                                    <img style={{
-                                        width:'10%',
-                                        height:'15%',
-                                        float:'right',
-                                        marginRight:'5%'
-                                    }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'onClick={this.change} alt='收藏' />
-                                </span>
-                            </div>
-                        </Flex.Item>
-                        <Flex.Item>
-                            <div style={{width:'100%'}}>
-                                <img src="https://s2.ax1x.com/2019/12/04/Q1fyjI.png" style={{width:'100%',height:'160px'}}/>
-                                <p style={{width:'100%'}}>
-                                    冬季腹泻，你的孩子中招了吗...<br/>
-                                </p>
-                                <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
-
-                                <span>
-                                    <img style={{
-                                        width:'10%',
-                                        height:'15%',
-                                        float:'right',
-                                        marginRight:'5%'
-                                    }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' onClick={this.change}alt='收藏' />
-                                </span>
-                            </div>
-                        </Flex.Item>
-                    </Flex> */}
-
-                   
-                </div>
-          
-           
+                <Flex>
+                    <Flex.Item>
+                        <div style={{width:'100%'}}>
+                            <img src="https://s2.ax1x.com/2019/12/04/Q1hib6.png" style={{width:'100%',height:'160px'}}/>
+                            <p style={{width:'100%',marginBottom:'2%'}}>
+                                宝宝从出生后至12岁，都要进行...<br/>
+                            </p>
+                            <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
+                            <span>
+                                <img style={{
+                                    width:'10%',
+                                    height:'15%',
+                                    float:'right',
+                                    marginRight:'5%'
+                                }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' alt='收藏' />
+                            </span>
+                        </div>
+                    </Flex.Item>
+                    <Flex.Item>
+                        <div style={{width:'100%'}}>
+                            <img src="https://s2.ax1x.com/2019/12/04/Q1hib6.png" style={{width:'100%',height:'160px'}}/>
+                            <p style={{width:'100%'}}>
+                                脊髓灰质炎糖丸—第...<br/>
+                            </p>
+                            <span style={{fontSize:'0.8em'}}>丫丫妈妈</span>
+                            <span>
+                                <img style={{
+                                    width:'10%',
+                                    height:'15%',
+                                    float:'right',
+                                    marginRight:'5%'
+                                }} src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png' alt='收藏' />
+                            </span>
+                        </div>
+                    </Flex.Item>
+                </Flex>
+            </div>
         )
     }
 }
