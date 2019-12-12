2d3910a1a13911d73c26781154bcde8ddac0aae5
shangchunli <1503651223@qq.com>
2019-12-12 19:52:44 +0800

    message

--- child-care/src/container/Sui.js
+++ child-care/src/container/Sui.js
@@ -1,84 +1,84 @@
-// import React, { Component } from 'react'
-// import { ImagePicker, WingBlank, SegmentedControl} from 'antd-mobile';
-// import  './sui.css';
-// const data = [];
-// export default class Sui extends Component {
-//     state = {
-//         files: data,
-//         multiple: false,
-
-//     }
-//     componentWillMount(){
-//       fetch('http://192.168.43.217:5001/add')
-//       .then(res=>res.text())
-//       .then((res)=>{
-//           console.log(res);
-//       })
-//   }
-//     onChange = (files, type, index) => {
-//         console.log(files, type, index);
-//         this.setState({
-//           files,
-//         });
-//       }
-//     onSegChange = (e) => {
-//         const index = e.nativeEvent.selectedSegmentIndex;
-//         this.setState({
-//           multiple: index === 1,
-//         });
-//     }
-   
-//     render() {
-//         const { files } = this.state;
-//         return (
-//             <div>
-//                  <div contentEditable="true" style={{marginLeft:100}}>
-//                 <p style={{paddingTop:'15%'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;记录美好瞬间...</p>
-//                 <WingBlank>
-//                         <ImagePicker
-//                         style={{paddingTop:'30%'}}
-//                         files={files}
-//                         onChange={this.onChange}
-//                         onImageClick={(index, fs) => console.log(index, fs)}
-//                         selectable={files.length < 100}
-//                         multiple={this.state.multiple}
-//                         />
-//                  </WingBlank>
-//                  </div>
-//                  <p style={{paddingTop:'5%'}}>......................................................................................................................</p>
-//                  <button className='sui1' style={{marginLeft:'23%'}}>发表</button>
-//             </div>
-//         )
-//     }
-// }
-
 import React, { Component } from 'react'
+import { ImagePicker, WingBlank, SegmentedControl} from 'antd-mobile';
 import  './sui.css';
+const data = [];
 export default class Sui extends Component {
-    constructor(){
-        super();
-        this.state={
-            data:''
-        }
+    state = {
+        files: data,
+        multiple: false,
+
     }
-    componentDidMount(){
-        fetch('http://192.168.43.217:5001/ppp')
-              .then(res=>res.text())
-              .then((res)=>{
-                  console.log(res);
-                  this.setState({
-                      data:res
-                  })
-              })
+    componentWillMount(){
+      fetch('http://192.168.43.217:5001/add')
+      .then(res=>res.text())
+      .then((res)=>{
+          console.log(res);
+      })
+  }
+    onChange = (files, type, index) => {
+        console.log(files, type, index);
+        this.setState({
+          files,
+        });
+      }
+    onSegChange = (e) => {
+        const index = e.nativeEvent.selectedSegmentIndex;
+        this.setState({
+          multiple: index === 1,
+        });
     }
+   
     render() {
-    
+        const { files } = this.state;
         return (
-            <div className="sui">
-                <div 
-                dangerouslySetInnerHTML={{ __html: this.state.data }}></div>
+            <div>
+                 <div contentEditable="true" style={{marginLeft:100}}>
+                <p style={{paddingTop:'15%'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;记录美好瞬间...</p>
+                <WingBlank>
+                        <ImagePicker
+                        style={{paddingTop:'30%'}}
+                        files={files}
+                        onChange={this.onChange}
+                        onImageClick={(index, fs) => console.log(index, fs)}
+                        selectable={files.length < 100}
+                        multiple={this.state.multiple}
+                        />
+                 </WingBlank>
+                 </div>
+                 <p style={{paddingTop:'5%'}}>......................................................................................................................</p>
+                 <button className='sui1' style={{marginLeft:'23%'}}>发表</button>
             </div>
         )
     }
 }
+
+// import React, { Component } from 'react'
+// import  './sui.css';
+// export default class Sui extends Component {
+//     constructor(){
+//         super();
+//         this.state={
+//             data:''
+//         }
+//     }
+//     componentDidMount(){
+//         fetch('http://192.168.43.217:5001/ppp')
+//               .then(res=>res.text())
+//               .then((res)=>{
+//                   console.log(res);
+//                   this.setState({
+//                       data:res
+//                   })
+//               })
+//     }
+//     render() {
+    
+//         return (
+//             <div className="sui">
+//                 <div 
+//                 dangerouslySetInnerHTML={{ __html: this.state.data }}></div>
+//             </div>
+//         )
+//     }
+// }
 
--- child-care/src/container/Tie.js
+++ child-care/src/container/Tie.js
@@ -1,79 +1,13 @@
 import React, { Component } from 'react'
-import { ImagePicker, WingBlank, SegmentedControl} from 'antd-mobile';
 import './sui.css';
 import Editor from './Add/Editor'
 import registerServiceWorker from './Add/registerServiceWorker'
-const data = [];
 
 export default class Sui extends Component {
-    state = {
-        files: data,
-        multiple: false,
-    }
-//     componentWillMount(){
-//       fetch('http://192.168.43.217:5001/add',{
-//               method: 'POST',//post请求 
-//           headers: { 
-//           'Content-Type': 'application/json;charset=UTF-8' 
-//           }, 
-//           body: JSON.stringify({
-//               title:"hello",
-//               content:"world"
-//           }
-//           )
-//       })
-//       .then(res=>res.text())
-//       .then((res)=>{
-//           console.log(res);
-//       })
-//   }
-    onChange = (files, type, index) => {
-        this.setState({
-          files,
-        });
-      }
-    onSegChange = (e) => {
-        const index = e.nativeEvent.selectedSegmentIndex;
-        this.setState({
-          multiple: index === 1,
-        });
-    }
-   
     render() {
-        // const { files } = this.state;
-        // console.log(files);
-        // let str;
-        // let obj = window.event.srcElement;
-        // if(JSON.stringify(files)!="[]"){
-        //     str=<img src={files[0].url} alt="Base64 encoded image"
-        //     height="200" width="200" style={{borderRadius:"80%",
-        //         width:"100%",height:"100%"
-        //     }}
-        //     />
-        // }
         return (
             <div >
                 <Editor/>
-                {/* <div contentEditable="true" ref>
-                   <p onClick={this.change} style={{marginLeft:"100px"}}>发表你的看法吧</p>
-
-                 <ImagePicker
-            style={{paddingTop:'30%'}}
-            files={files}
-            onChange={this.onChange}
-            
-            selectable={files.length < 100}
-            multiple={this.state.multiple}
-            />
-                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;标签：
-                 <button className='tie1' >潮娃穿搭</button>
-                 <button className='tie1' >常见疾病</button>
-                 <button className='tie1' >亲子教育</button>
-                 ....
-                 <p style={{paddingTop:'5%'}}>......................................................................................................................</p>
-            </div>
-            <button className='sui1' style={{marginLeft:'23%'}}>发表</button> */}
-
             </div>
         )
     }
