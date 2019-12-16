167d3abdfa1674afe9089b2a1667c1c69ab2c2b0
shangchunli <1503651223@qq.com>
2019-12-16 22:33:29 +0800

    message

--- child-care/src/container/wiki/Vaccin.js
+++ child-care/src/container/wiki/Vaccin.js
@@ -5,9 +5,7 @@ export default class Vaccin extends Component {
         return (
             <div>
                 Vaccin
-                <h1>
-                    
-                </h1>
+                
             </div>
         )
     }
