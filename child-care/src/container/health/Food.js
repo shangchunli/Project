2894046071faea9c500d24ae65869ba9863c8660
changyanxin <2873005857@qq.com>
2019-12-17 18:51:08 +0800

    x

--- child-care/src/container/health/Food.js
+++ child-care/src/container/health/Food.js
@@ -1,9 +1,73 @@
 import React, { Component } from 'react'
-import { WhiteSpace,NavBar,Icon } from 'antd-mobile';
+import cookie from 'react-cookies'
+import {withRouter} from 'react-router-dom'
+
+import { WhiteSpace,NavBar,Icon,List } from 'antd-mobile';
 // import { List } from 'antd-mobile';
-export default class Food extends Component {
+class Food extends Component {
+    constructor(){
+        super();
+        this.state={
+            data:[],
+            isKeep:false,
+            chapterId:[],
+            userId:cookie.load('userId')
+        }
+    }
+    componentDidMount(){
+        //初始加载
+        fetch("http://192.168.43.217:5001/momfood")
+        .then(res=>res.json())
+        .then((res)=>{
+            this.setState({
+                data:res
+            })
+        })
+    }
+    componentDidUpdate(){
+        fetch("http://192.168.43.217:5001/momfood")
+        .then(res=>res.json())
+        .then((res)=>{
+            console.log(res)
+        })
+    }
+    change1=(idx,e)=>{
+        e.stopPropagation();
+        let b=this.state.isKeep;
+        this.setState({
+            isKeep:!b,
+            chapterId:idx
+
+        })
+        console.log(this.state.chapterId);
+        
+        if(this.state.isKeep==true){
+             e.target.src='https://s2.ax1x.com/2019/12/11/QrKe4s.png'
+                fetch('http://192.168.43.217:5001/cookie',{
+                        method: 'POST',//post请求 
+                        headers: { 
+                        'Content-Type': 'application/json;charset=UTF-8' 
+                        }, 
+                        body: JSON.stringify({
+                            userId:this.state.userId,  
+                            chapterId:this.state.chapterId                    
+                        })                    
+                })
+                .then(res=>res.json())
+                .then((res)=>{
+                    console.log(res);
+                   
+                })
+         }else{
+            e.target.src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'
+        }
+        // console.log(this.state.chapterId);
+    }
     goBack=()=>{
-        window.history.go(-1);
+        this.props.history.go(-1);
+    }
+    too=(id)=>{
+        this.props.history.push('/health/detail/'+id);
     }
     render() {
         return (
@@ -18,58 +82,33 @@ export default class Food extends Component {
                     >
                         饮食
                 </NavBar>
-                <WhiteSpace size="sm" />
-                <div className='placeholder1'>
-                    <img src='https://s2.ax1x.com/2019/12/04/Q1HiSU.jpg
-'
-                        style={{width:'17%',height:'92%'}}
-                    />
-                    &nbsp;&nbsp;&nbsp;哺乳期怎么吃？
-                </div>
+                {
+                            (this.state.data||[]).map(item=>
+                                <List style={{marginTop:10}} id={item.chapterid} 
+                                onClick={()=>this.too(item.chapterid)}>
+                                <List.Item style={{paddingTop:10,
+                                    color:"#000"}}>
+                                    <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg"
+                                             style={{float:"right",
+                                        width:50,height:70,paddingBottom:10}}/>
+                                        {item.title}
+                                    <List.Item.Brief style={{paddingTop:10}}>{item.owner}
+                                        <span style={{marginLeft:10,fontSize:'10px'}}>{item.time}</span>
+                                        <span  style={{marginLeft:20}} onClick={()=>{}}>
+                                        <img src={(cookie.load('chapterId'))
+                                            ?'https://s2.ax1x.com/2019/12/11/QrKe4s.png'
+                                            :"https://s2.ax1x.com/2019/12/04/Q1fu7T.png"}
+                                            onClick={(e)=>this.change1(item.chapterid,e)} alt='收藏'/>
+                                        </span>
+                                    </List.Item.Brief>
+                                </List.Item>
+                            </List>
 
-                <WhiteSpace size="sm" />
-                <div className='placeholder1'>
-                    <img src='https://s2.ax1x.com/2019/12/04/Q1HiSU.jpg
-'
-                        style={{width:'17%',height:'92%'}}
-                    />
-                    &nbsp;&nbsp;&nbsp;让母乳更有营养，怎么吃？
-                </div>
-                <WhiteSpace size="sm" />
-                <div className='placeholder1'>
-                    <img src='https://s2.ax1x.com/2019/12/04/Q1HiSU.jpg
-'
-                        style={{width:'17%',height:'92%'}}
-                    />
-                    &nbsp;&nbsp;&nbsp;哺乳期妈妈可不可以吃药？
-                </div>
-                <WhiteSpace size="sm" />
-                <div className='placeholder1'>
-                    <img src='https://s2.ax1x.com/2019/12/04/Q1HiSU.jpg
-'
-                        style={{width:'17%',height:'92%'}}
-                    />
-                    &nbsp;&nbsp;&nbsp;女人做月子到底可以吃什么蔬菜？
-                </div>
-                <WhiteSpace size="sm" />
-                <div className='placeholder1'>
-                    <img src='https://s2.ax1x.com/2019/12/04/Q1HiSU.jpg
-'
-                        style={{width:'17%',height:'92%'}}
-                    />
-                    &nbsp;&nbsp;&nbsp;不为人知的剖腹产妇饮食注意事项
-                </div>
-                <WhiteSpace size="sm" />
-                <div className='placeholder1'>
-                    <img src='https://s2.ax1x.com/2019/12/04/Q1HiSU.jpg
-'
-                        style={{width:'17%',height:'92%'}}
-                    />
-                    &nbsp;&nbsp;&nbsp;坐月子饮食禁忌，你注意到了吗？
-                </div>
-                
-                
+                            )
+                                            
+                }                
             </div>
         )
     }
 }
+export default withRouter(Food);
--- child-care/src/container/health/Reply.js
+++ child-care/src/container/health/Reply.js
@@ -1,13 +1,80 @@
 import React, { Component } from 'react'
-
+import cookie from 'react-cookies'
+import {withRouter} from 'react-router-dom'
 import { NavBar,List,Icon } from 'antd-mobile';
 
 const Item = List.Item;
 
 
-export default class Reply extends Component {
+class Reply extends Component {
+    constructor(){
+        super();
+        this.state={
+            data:[],
+            isKeep:false,
+            chapterId:[],
+            userId:cookie.load('userId')
+        }
+    }
+    componentDidMount(){
+        //初始加载
+        fetch("http://192.168.43.217:5001/huli")
+        .then(res=>res.json())
+        .then((res)=>{
+            this.setState({
+                data:res
+            })
+        })
+    }
+    componentDidUpdate(){
+        fetch("http://192.168.43.217:5001/huli")
+        .then(res=>res.json())
+        .then((res)=>{
+            console.log(res)
+        })
+    }
+    change1=(idx,e)=>{
+        e.stopPropagation();
+        let b=this.state.isKeep;
+        this.setState({
+            isKeep:!b,
+            chapterId:idx
+
+        })
+        console.log(this.state.chapterId);
+        
+        if(this.state.isKeep==true){
+            
+            e.target.src='https://s2.ax1x.com/2019/12/11/QrKe4s.png'
+            // alert(this.state.userId);
+           
+                fetch('http://192.168.43.217:5001/cookie',{
+                        method: 'POST',//post请求 
+                        headers: { 
+                        'Content-Type': 'application/json;charset=UTF-8' 
+                        }, 
+                        body: JSON.stringify({
+                            userId:this.state.userId,  
+                            chapterId:this.state.chapterId                    
+                        })                    
+                })
+                .then(res=>res.json())
+                .then((res)=>{
+                    console.log(res);
+                   
+                })
+               
+           
+        }else{
+            e.target.src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'
+        }
+        // console.log(this.state.chapterId);
+    }
     goBack=()=>{
-        window.history.go(-1);
+       this.props.history.go(-1);
+    }
+    too=(id)=>{
+        this.props.history.push('/health/detail/'+id);
     }
     render() {
         return (
@@ -21,120 +88,35 @@ export default class Reply extends Component {
                     >
                         身材恢复
                 </NavBar>
-                <div style={{marginTop:10,height:600
-                ,width:"100%",
-                    }}>
-                    <List>
-                        <List.Item style={{paddingTop:10,
-                            color:"#000"}}>
-                            <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg" style={{float:"right",
-                                width:50,height:70,paddingBottom:10}}/>
-                                产后瘦身关键期
-                                <List.Item.Brief style={{color:"#000"}}>
-                                    产后六个月是减重的黄金期</List.Item.Brief>
-                            <List.Item.Brief style={{paddingTop:10}}>丫丫妈妈
-                                <span style={{marginLeft:10}}>11月12日</span>
-                                <span  style={{marginLeft:20}} onClick={()=>{}}>
-                                    <img src="https://s2.ax1x.com/2019/12/04/Q1fu7T.png"/>
-                                </span>
-                            </List.Item.Brief>
-                        </List.Item>
-                    </List>
-                    <List style={{marginTop:10}}>
-                        <List.Item style={{paddingTop:10}}>
-                            <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg" style={{float:"right",
-                                width:50,height:70,paddingBottom:10}}/>
-                                产后身体出现这4种情况,说明<br/>你的月子没坐好!
-                                <List.Item.Brief style={{color:"#000"}}>
-                                都说月子坐好了，能让女人脱胎换骨。怎么坐月子，谁照顾月子？
-                                </List.Item.Brief>
-                            <List.Item.Brief style={{paddingTop:10}}>丫丫妈妈
-                                <span style={{marginLeft:10}}>11月12日</span>
-                                <span  style={{marginLeft:20}} onClick={()=>{}}>
-                                    <img src="https://s2.ax1x.com/2019/12/04/Q1fu7T.png"/>
-                                </span>
-                            </List.Item.Brief>
-                        </List.Item>
-                    </List>
-                    <List style={{marginTop:10}}>
-                        <List.Item style={{paddingTop:10}}>
-                            <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg" style={{float:"right",
-                                width:50,height:70,paddingBottom:10}}/>
-                                妊娠纹怎么办
-                               <List.Item.Brief style={{color:"#000"}}
-                               >妊娠纹一旦形成，是不可能完全去除的，但是可以随着时间的推移，而逐渐淡化。</List.Item.Brief> 
+                <div style={{marginTop:10,height:600,width:"100%"}}>
+                {
+                            (this.state.data||[]).map(item=>
+                                <List style={{marginTop:30}} id={item.chapterid} 
+                                onClick={()=>this.too(item.chapterid)}>
+                                <List.Item style={{paddingTop:10,
+                                    color:"#000"}}>
+                                    <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg"
+                                             style={{float:"right",
+                                        width:50,height:70,paddingBottom:10}}/>
+                                        {item.title}
+                                    <List.Item.Brief style={{paddingTop:10}}>{item.owner}
+                                        <span style={{marginLeft:10,fontSize:'10px'}}>{item.time}</span>
+                                        <span  style={{marginLeft:20}} onClick={()=>{}}>
+                                        <img src={(cookie.load('chapterId'))
+                                            ?'https://s2.ax1x.com/2019/12/11/QrKe4s.png'
+                                            :"https://s2.ax1x.com/2019/12/04/Q1fu7T.png"}
+                                            onClick={(e)=>this.change1(item.chapterid,e)} alt='收藏'/>
+                                        </span>
+                                    </List.Item.Brief>
+                                </List.Item>
+                            </List>
 
-                            <List.Item.Brief style={{paddingTop:10}}>丫丫妈妈
-                                <span style={{marginLeft:10}}>11月12日</span>
-                                <span  style={{marginLeft:20}} onClick={()=>{}}>
-                                    <img src="https://s2.ax1x.com/2019/12/04/Q1fu7T.png"/>
-                                </span>
-                            </List.Item.Brief>
-                        </List.Item>
-                    </List>
-                    <List style={{marginTop:10}}>
-                        <List.Item style={{paddingTop:10}}>
-                            <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg" style={{float:"right",
-                                width:50,height:70,paddingBottom:10}}/>
-                               妊娠纹要长躲不过,三个方法,帮助预防妊娠纹
-                                <List.Item.Brief style={{color:"#000"}}>
-                                怀孕生娃除了怕胖、身材走样，最怕的就是长妊娠纹了。</List.Item.Brief>
-                            <List.Item.Brief style={{paddingTop:10}}>丫丫妈妈
-                                <span style={{marginLeft:10}}>11月12日</span>
-                                <span  style={{marginLeft:20}} onClick={()=>{}}>
-                                    <img src="https://s2.ax1x.com/2019/12/04/Q1fu7T.png"/>
-                                </span>
-                            </List.Item.Brief>
-                        </List.Item>
-                    </List>
-                    <List style={{marginTop:10}}>
-                        <List.Item style={{paddingTop:10}}>
-                            <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg" style={{float:"right",
-                                width:50,height:70,paddingBottom:10}}/>
-                                产后身材恢复瑜伽怎么做
-                                <List.Item.Brief style={{color:"#000"}}>
-                                产后瑜伽能帮助产后妈妈恢复身体，也可以促进血液循环，紧实肌肉，帮助卵巢正常发挥功能。</List.Item.Brief>
-                            <List.Item.Brief style={{paddingTop:10}}>丫丫妈妈
-                                <span style={{marginLeft:10}}>11月12日</span>
-                                <span  style={{marginLeft:20}} onClick={()=>{}}>
-                                    <img src="https://s2.ax1x.com/2019/12/04/Q1fu7T.png"/>
-                                </span>
-                            </List.Item.Brief>
-                        </List.Item>
-                    </List>
-                    <List style={{marginTop:10}}>
-                        <List.Item style={{paddingTop:10}}>
-                            <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg" style={{float:"right",
-                                width:50,height:70,paddingBottom:10}}/>
-                                产后如何保健
-                                <List.Item.Brief style={{color:"#000"}}>
-                                如果保健没有做好，产妇以后可能会比身体健康的女性更容易染上妇科病</List.Item.Brief>
-                            <List.Item.Brief style={{paddingTop:10}}>丫丫妈妈
-                                <span style={{marginLeft:10}}>11月12日</span>
-                                <span  style={{marginLeft:20}} onClick={()=>{}}>
-                                    <img src="https://s2.ax1x.com/2019/12/04/Q1fu7T.png"/>
-                                </span>
-                            </List.Item.Brief>
-                        </List.Item>
-                    </List>
-                    <List style={{marginTop:10}} onClick={()=>{console.log("11")}}>
-                        <List.Item style={{paddingTop:10}}>
-                            <img src="https://s2.ax1x.com/2019/12/04/Q1N84U.jpg" style={{float:"right",
-                                width:50,height:70,paddingBottom:10}}/>
-                                产后修复体操的做法
-                                <List.Item.Brief style={{color:"#000"}}>
-                                通过活动和锻炼，可逐步恢复体型，节食虽然操作起来简单，
-                                但是对于产妇身体的伤害非常大，严重还会有后遗症</List.Item.Brief>
-                            <List.Item.Brief style={{paddingTop:10}}>丫丫妈妈
-                                <span style={{marginLeft:10}}>11月12日</span>
-                                <span  style={{marginLeft:20}} onClick={()=>{}}>
-                                    <img src="https://s2.ax1x.com/2019/12/04/Q1fu7T.png"/>
-                                </span>
-                            </List.Item.Brief>
-                        </List.Item>
-                    </List>
+                            )
+                                            
+                        }
                 </div>
             </div>
         )
     }
 }
+export default withRouter(Reply);
