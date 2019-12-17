bedc4fa7c636572e3b6d11d360cb00633d47ec17
changyanxin <2873005857@qq.com>
2019-12-17 10:15:55 +0800

    x

--- child-care/src/container/health/PregnaceCare.js
+++ child-care/src/container/health/PregnaceCare.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react'
+import cookie from 'react-cookies'
+
 
 import { NavBar,List,Icon } from 'antd-mobile';
 
 const Item = List.Item;
 
-export default class Yiyu extends Component {
+export default class PregnaceCare extends Component {
     constructor(){
         super();
         this.state={
-            data:[]
+            data:[],
+            isKeep:false,
+            chapterId:[],
+            userId:cookie.load('userId')
         }
     }
     componentDidMount(){
         //初始加载
-        fetch("http://192.168.43.217:5001/yiyu")
+        fetch("http://192.168.43.217:5001/huli")
         .then(res=>res.json())
         .then((res)=>{
             this.setState({
@@ -22,14 +27,54 @@ export default class Yiyu extends Component {
         })
     }
     componentDidUpdate(){
-        fetch("http://192.168.43.217:5001/yiyu")
+        fetch("http://192.168.43.217:5001/huli")
         .then(res=>res.json())
         .then((res)=>{
             console.log(res)
         })
     }
+    change1=(idx,e)=>{
+        e.stopPropagation();
+        let b=this.state.isKeep;
+        this.setState({
+            isKeep:!b,
+            chapterId:idx
+
+        })
+        console.log(this.state.chapterId);
+        
+        if(this.state.isKeep==true){
+            
+            e.target.src='https://s2.ax1x.com/2019/12/11/QrKe4s.png'
+            // alert(this.state.userId);
+           
+                fetch('http://192.168.43.217:5001/cookie',{
+                        method: 'POST',//post请求 
+                        headers: { 
+                        'Content-Type': 'application/json;charset=UTF-8' 
+                        }, 
+                        body: JSON.stringify({
+                            userId:this.state.userId,  
+                            chapterId:this.state.chapterId                    
+                        })                    
+                })
+                .then(res=>res.json())
+                .then((res)=>{
+                    console.log(res);
+                   
+                })
+               
+           
+        }else{
+            e.target.src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png'
+        }
+        // console.log(this.state.chapterId);
+    }
     goBack=()=>{
         window.history.go(-1);
+    }
+    too=(id)=>{
+        window.location.href='/health/detail/'+id;
     }
     render() {
         return (
@@ -44,11 +89,9 @@ export default class Yiyu extends Component {
                     width:"100%"
                 }}
                 >
-                    抑郁症
+                    孕期护理
                 </NavBar>
-                <div style={{marginTop:50,height:600
-                ,width:"100%",
-                    }}>
+                <div style={{marginTop:50,height:600,width:"100%"}}>
                         {
                             (this.state.data||[]).map(item=>
                                 <List style={{marginTop:30}} id={item.chapterid} 
@@ -60,9 +103,12 @@ export default class Yiyu extends Component {
                                         width:50,height:70,paddingBottom:10}}/>
                                         {item.title}
                                     <List.Item.Brief style={{paddingTop:10}}>{item.owner}
-                                        <span style={{marginLeft:10}}>{item.time}</span>
+                                        <span style={{marginLeft:10,fontSize:'10px'}}>{item.time}</span>
                                         <span  style={{marginLeft:20}} onClick={()=>{}}>
-                                            <img src="https://s2.ax1x.com/2019/12/04/Q1fu7T.png"/>
+                                        <img src={(cookie.load('chapterId'))
+                                            ?'https://s2.ax1x.com/2019/12/11/QrKe4s.png'
+                                            :"https://s2.ax1x.com/2019/12/04/Q1fu7T.png"}
+                                            onClick={(e)=>this.change1(item.chapterid,e)} alt='收藏'/>
                                         </span>
                                     </List.Item.Brief>
                                 </List.Item>
