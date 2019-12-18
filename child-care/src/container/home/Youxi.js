99d662def46aefe12f335494caa5d47c7952296c
yinmeili <2697111697@qq.com>
2019-12-18 08:36:17 +0800

    messa

--- child-care/src/container/home/Youxi.js
+++ child-care/src/container/home/Youxi.js
@@ -7,6 +7,9 @@ export default class Youxi extends Component {
     goBack=()=>{
         window.history.go(-1);
     }
+    handle = (pathname) => {
+        this.props.history.push( "/wiki/" + pathname);
+    }
     render() {
         return (
             <ul>
@@ -25,49 +28,53 @@ export default class Youxi extends Component {
                 <li style={{marginTop:'20%',marginLeft:'9%'}}>    
                     <div className='tiezi'style={{ }}>
                         {/* <img  src='./images/chaowa/youxi1.png'/> */}
-                        <img  src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/> 
+                        <img  onClick={() => { this.handle('youxi1') }}src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/> 
 
-                    <p>寿司卷--棉被小道具</p>
-                    <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
+                    <p>爸爸妈妈（4-6岁）</p>
+                    {/* <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
                          src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png
-                         ' />
+                         ' /> */}
                     </div>
                     <div className='tiezi'style={{ }}>
                     {/* <img  src='./images/chaowa/youxi2.png'/> */}
-                    <img  src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
-                    <p>荡秋千--棉被小道具</p>
-                    <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
+                    <img onClick={() => { this.handle('youxi2') }} src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
+                    <p>小心陷阱（4-5岁）</p>
+                    {/* <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
                          src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png
-                         ' />
+                         ' /> */}
                     </div>
                     <div className='tiezi'style={{}}>
                     {/* <img  src='./images/chaowa/youxi1.png'/> */}
-                    <img  src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
-                    <p>拉雪橇--棉被小道具</p>
-                    <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
+                    <img  onClick={() => { this.handle('youxi3') }}src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
+                    <p>大西瓜小西瓜（5-6岁）</p>
+                    {/* <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
                          src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png
-                         ' />                    </div>
+                         ' />                    */}
+                    </div>
                     <div className='tiezi'style={{}}>
                     {/* <img  src='./images/chaowa/youxi2.png'/> */}
-                    <img  src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
-                    <p>捞金鱼--脸盆小道具</p>
-                    <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
+                    <img onClick={() => { this.handle('youxi4') }} src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
+                    <p>"二人三足跑"（５－６岁）</p>
+                    {/* <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
                          src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png
-                         ' />                    </div>
+                         ' />                     */}
+                    </div>
                     <div className='tiezi'style={{ }}>
                     {/* <img  src='./images/chaowa/youxi2.png'/> */}
-                    <img  src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
-                    <p>泡澡好好玩--脸盆小道具</p>
-                    <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
+                    <img  onClick={() => { this.handle('youxi5') }}src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
+                    <p>"传达室动作"（4－６岁）</p>
+                    {/* <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
                          src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png
-                         ' />                    </div>
+                         ' />                     */}
+                    </div>
                     <div className='tiezi'style={{ }}>
                     {/* <img  src='./images/chaowa/youxi2.png'/> */}
-                    <img  src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
-                    <p>小小模特儿--脸盆小道具</p>
-                    <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
+                    <img onClick={() => { this.handle('youxi6') }} src='https://s2.ax1x.com/2019/12/04/Q1YuGQ.png'/>
+                    <p>"毛毛虫"（4－６岁）</p>
+                    {/* <img style={{height:20,width:20,float:'right',marginTop:-30,marginRight:10}} 
                          src='https://s2.ax1x.com/2019/12/04/Q1fu7T.png
-                         ' />                    </div>
+                         ' />                    */}
+                    </div>
 
                 </li>
             </ul>
