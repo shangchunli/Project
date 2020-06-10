c24f5c38fb630a477f03764bd16cd7e193040e1c
shangchunli <1503651223@qq.com>
2020-06-11 06:28:42 +0800

    index

--- DataExpress/routes/index.js
+++ DataExpress/routes/index.js
@@ -835,7 +835,6 @@ router.get('/fenye2',function(req,res,next){
       console.log(err);
     }
     else{
-      console.log(result.length);
       res.render('chapter',{usersList:result})
     }
   })
