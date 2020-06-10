4b62149eac7d85e5ce2fd29d2ccedd8361b7a3d3
Zhangjie8961 <33643034+Zhangjie8961@users.noreply.github.com>
2020-06-10 23:51:39 +0800

    17

--- DataExpress/routes/users.js
+++ DataExpress/routes/users.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 var path = require('path
 //引入数据库配置文件，这个文件是自己建的
-// var child-care = require('../config/child-care.json');
 
 /* GET home page. */
   res.render('index', { title: 'Express' });
