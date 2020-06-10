d8dcdcc7f35ee42bfaf2c7868d90f162593e37be
常延欣 <33642984+changyanxin@users.noreply.github.com>
2020-06-10 22:54:30 +0800

    Update Add.js

--- child-care/src/container/Add.js
+++ child-care/src/container/Add.js
@@ -24,7 +24,7 @@ export default class Add extends Component {
                
                 
               
-                {/* <Unique/> */}
+               
                 {/* <BabyFood/> */}
                 {/* <Edu/> */}
                 {/* <Food/> */}
