3aa327f4deeaf3a72db26a3767311a62b805debc
银美丽 <33643062+yinmeili@users.noreply.github.com>
2020-06-10 23:45:18 +0800

    Update AppHome.js

--- child-care/src/container/AppHome.js
+++ child-care/src/container/AppHome.js
@@ -14,7 +14,7 @@ class AppHome extends Component {
     }
     componentDidMount(){
         // console.log(this.props.match.params.id);
-//         在首页获取随想61
+//         在首页获取随想62
         fetch('http://localhost:5001/caprice',{
             method: 'POST',//post请求 
             headers: { 
