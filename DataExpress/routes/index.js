6adffe2fb5a0be3c6e356ad55fb7e4c032308901
shangchunli <1503651223@qq.com>
2020-06-11 07:04:40 +0800

    index

--- DataExpress/routes/index.js
+++ DataExpress/routes/index.js
@@ -38,7 +38,6 @@ router.post('/yhlogin',function(req,res,next){
 //用户注册提交上的信息
 // 头像：图片选择器得到的base64编码，去除头部信息，转存到本地服务器，
 router.post('/register',function(req,res,next){
-  // res.send('register');
   console.log('register')
    var name =req.body.names;
    var telphone = req.body.tel;
